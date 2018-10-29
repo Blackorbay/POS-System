9fe2985edebc4eaa334b131c026a3d5055646d71
wingjl28 <43913004+wingjl28@users.noreply.github.com>
2018-10-29 15:30:42 -0500

    Update InventoryManager.java

--- src/InventoryManager.java
+++ src/InventoryManager.java
@@ -3,6 +3,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.io.FileWriter;
 import java.io.File;
 
@@ -13,15 +14,15 @@ public class InventoryManager
 	private String itemSup;
 	private float itemCost;
 	private int itemQuant;
-  private Item tempItem; 
+	private Item tempItem; 
 	private boolean exists;
 	private String filename = "C:\\Users\\Wingman\\eclipse-workspace\\POS System\\bin\\InventoryItems.txt";
-	
-	
+	private ArrayList<Item> itemInventory = new ArrayList<Item>(1);
 
 	InventoryManager()
 	{
 		File inventory = new File(filename);
+		populateInventory();
 	}
   
   //specify filename for different inventories.
@@ -29,12 +30,17 @@ public class InventoryManager
   {
     this.filename = filename;
     File inventory = new File(filename);
+    
+    populateInventory();
+    
+    
+    
   }
 	
 	private boolean checkItemInFile(Item testItem)
 	{
 		
-		boolean itemFound = false;
+	boolean itemFound = false;
     String line = "";
     String cvsSplitBy = ",";
     BufferedReader br = null;
@@ -87,7 +93,7 @@ public class InventoryManager
 
              // use comma as separator
              String[] tempInventory = line.split(cvsSplitBy);
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < 6; i++)
               System.out.print(" " + tempInventory[i]);
            System.out.println();
 
@@ -124,6 +130,8 @@ public class InventoryManager
 	        sb.append(newItem.getSup());
 	        sb.append(',');
 	        sb.append(Integer.toString(newItem.getCount()));
+	        sb.append(',');
+	        sb.append(Integer.toString(newItem.getThreshold()));
 	        sb.append(',');
 	        sb.append(Float.toString(newItem.getCost()));
 	        sb.append('\n');
@@ -173,6 +181,8 @@ public class InventoryManager
 	        sb.append(tempInventory[3]);
 	        sb.append(',');
 	        sb.append(tempInventory[4]);
+	        sb.append(',');
+	        sb.append(tempInventory[5]);
 	        sb.append('\n');
 
 	        pw.append(sb.toString());
@@ -212,35 +222,83 @@ public class InventoryManager
     
     
   }
+  
+	private void populateInventory()
+	{
+		Item tempItem; 
+	    String line = "";
+	    String cvsSplitBy = ",";
+	    BufferedReader br = null;
+	 
+	    try {
+			 br = new BufferedReader(new FileReader(filename));
+	         while ((line = br.readLine()) != null) {
+
+	             // use comma as separator
+	             String[] tempInventory = line.split(cvsSplitBy);
+	             
+	             tempItem = new Item(tempInventory[0], Float.parseFloat(tempInventory[5]), Integer.parseInt(tempInventory[3]), Integer.parseInt(tempInventory[4]), tempInventory[2]);
+	             itemInventory.add(tempItem);
+	         }
+			 
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        } finally {
+	            if (br != null) {
+	                try {
+	                    br.close();
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        }
+	
+	}
+  
+  public String nightlyRun()
+  {
+	  String purchaseItems = "Item ID's in need of restock are: ";
+	  
+	  for (int i=0; i<itemInventory.size(); i++)
+	  {
+		  if (itemInventory.get(i).getCount() < itemInventory.get(i).getThreshold())
+			  purchaseItems += ("\n" + itemInventory.get(i).toString());
+			  //purchaseItems += (itemInventory.get(i).getID() + ", ");
+	  }
+	  
+	  return purchaseItems;
+  }
 	
 	//TODO: for testing and debugging purposes only remove once done with project
 	public static void main(String[] args) 
 	{
  	InventoryManager inventory = new InventoryManager();
-//    Item testItemOne = new Item("One",11.11f, 12, "Costco");
-//    Item testItemTwo = new Item("Two",11.11f, 12, "Walmart");
+//    Item testItemOne = new Item("One",11.11f, 12,5, "Costco");
+//    Item testItemTwo = new Item("Two",11.11f, 12,3, "Walmart");
 //    Item testItemThree = new Item("Three",11.11f, 12, "McDonalds");
 //    Item testItemFour = new Item("Four",11.11f, 12, "Target");
 //    Item testItemFive = new Item("Five",11.11f, 12, "Cubs");
 //    Item testItemSix = new Item("Six",11.11f, 12, "Lunds");
-    
+//    
 //    inventory.addNewToInventory(testItemOne);
 //    inventory.addNewToInventory(testItemTwo);
 //    inventory.addNewToInventory(testItemThree);
 //    inventory.addNewToInventory(testItemFour);
 //    inventory.addNewToInventory(testItemFive);
 //    inventory.addNewToInventory(testItemSix);
-//    
-//    inventory.displayInventoryReport();
- 	
- 	Item updateOne = new Item("67111115791101010", 2);
- 	Item updateTwo = new Item("779968841041142", 2);
-    inventory.updateInventory(updateOne, 9999);
-    inventory.updateInventory(updateTwo, 9999);
+    
     inventory.displayInventoryReport();
+ 	System.out.println(inventory.nightlyRun());
+// 	Item updateOne = new Item("67111115791101010", 2);
+// 	Item updateTwo = new Item("779968841041142", 2);
+//    inventory.updateInventory(updateOne, 9999);
+//    inventory.updateInventory(updateTwo, 9999);
+//    inventory.displayInventoryReport();
   }
 	
-	
+
 
 
   
